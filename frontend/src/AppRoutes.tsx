3457d39682289c39035670c743a4e21bc60d9a16
Abhishek <abhitiku2003@gmail.com>
2024-03-25 11:40:11 +0530

    landing page done

---
+++ frontend/src/AppRoutes.tsx
@@ -0,0 +1,15 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./layouts/Layout";
+import Homepage from "./pages/Homepage";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout><Homepage /></Layout>} />
+      <Route path="/user-profile" element={<span>User Profile</span>} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import { NextUIProvider } from "@nextui-org/react";
+
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./AppRoutes";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <NextUIProvider>
-      <App />
+      <Router>
+        <AppRoutes />
+      </Router>
     </NextUIProvider>
   </React.StrictMode>
 );
