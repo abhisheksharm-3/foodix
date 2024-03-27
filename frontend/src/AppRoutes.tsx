efb4eabd4f35278144f0bf2c105d55c77fd0cce4
Abhishek <abhitiku2003@gmail.com>
2024-03-27 17:31:05 +0530

    added access token in login flow

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,11 +1,20 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Homepage from "./pages/Homepage";
+import AuthCallbackPage from "./pages/AuthCallbackPage";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout><Homepage /></Layout>} />
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <Homepage />
+          </Layout>
+        }
+      />
+      <Route path="/auth-callback" element={<AuthCallbackPage />} />
       <Route path="/user-profile" element={<span>User Profile</span>} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
