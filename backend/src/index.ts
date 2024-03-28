968950da0aa53abfbdc0f0d50aa8109ea54c8b94
Abhishek <abhitiku2003@gmail.com>
2024-03-28 13:40:22 +0530

    health endpoint added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,6 +13,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "Server Health is Good" });
+});
+
 app.use("/api/my/user", myUserRoute)
 
 app.listen(7000, () => {
