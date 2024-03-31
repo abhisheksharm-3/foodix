7c7b30af88995f452c9274b8cb52d7445a706229
Abhishek <abhitiku2003@gmail.com>
2024-03-31 13:38:22 +0530

    added restaurant API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,10 +3,18 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
+import { v2 as cloudinary } from "cloudinary";
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => console.log("Connected to DB"));
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 
@@ -17,7 +25,8 @@ app.get("/health", async (req: Request, res: Response) => {
   res.send({ message: "Server Health is Good" });
 });
 
-app.use("/api/my/user", myUserRoute)
+app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
 
 app.listen(7000, () => {
   console.log("Server Started on 7000 Port");
