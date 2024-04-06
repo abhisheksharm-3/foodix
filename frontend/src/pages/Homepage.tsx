ab99566ca6196d2b18b26abe9bb416ccdac4fe8d
Abhishek <abhitiku2003@gmail.com>
2024-04-06 13:01:20 +0530

    added search api and searchba

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,13 +1,25 @@
 import landingImage from "@/assets/landing.png";
 import appDownload from "@/assets/appDownload.png";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+import { useNavigate } from "react-router-dom";
 const Homepage = () => {
+  const navigate = useNavigate();
+  const handleSearchSubmit = (searchFormValues: SearchForm) => {
+    navigate({
+      pathname: `/search/${searchFormValues.searchQuery}`,
+    });
+  };
   return (
     <div className="flex flex-col gap-12">
-      <div className="bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
+      <div className="bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16 md:px-32">
         <h1 className="text-5xl font-bold tracking-tight text-orange-600">
-          Tuck into takwaway today
+          Tuck into takwaway today!
         </h1>{" "}
         <span className="text-xl ">Food is just a Click away!</span>
+        <SearchBar
+          placeHolder="Search by City or Town"
+          onSubmit={handleSearchSubmit}
+        />
       </div>
       <div className="grid md:grid-cols-2 gap-5">
         <img src={landingImage} />
